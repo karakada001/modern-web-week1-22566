7cc9ef8bbcb3cfa213e10096b3ea490586487dda
Khunaiko <pasuaiko21089@gmail.com>
2023-10-17 15:46:12 +0700

    first commit

---
+++ app5.js
@@ -0,0 +1,12 @@
+//Basic syntag od String
+let msg = "Hello";
+let fnam = "Mark";
+let lname = "Zuckerderg";
+let age = "10" + 10; //1010
+let salaray = "10" - 7; //3
+let money = "Hello" - 10; // Nan
+console.log(msg + " " + fnam + " " + lname + " " +  age );
+console.log(salaray + " " + money );
+console.log(lname.length);
+console.log(lname.toUpperCase());
+console.log(lname.toLocaleLowerCase());
